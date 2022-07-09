e73042662526ed6bc26c65d83b4d55c7a5b5d05d
shabnam1999 <56621170+shabnam1999@users.noreply.github.com>
2022-07-09 13:07:48 +0530

    initial commit

---
+++ customeFramework/customeFramework/customeFramework.h
@@ -0,0 +1,18 @@
+//
+//  customeFramework.h
+//  customeFramework
+//
+//  Created by DREAMWORLD on 09/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for customeFramework.
+FOUNDATION_EXPORT double customeFrameworkVersionNumber;
+
+//! Project version string for customeFramework.
+FOUNDATION_EXPORT const unsigned char customeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <customeFramework/PublicHeader.h>
+
+
